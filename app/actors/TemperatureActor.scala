2178c902d0a038e681310e95fac0f915c8a49c7f
Russ Pridemore <russ.pridemore@gmail.com>
2016-09-27 23:08:24 -0400

    Refactor actor class to use Futures to read temps in parallel.  Working, but not pretty.  Sure there must be a cleaner way.

--- app/actors/TemperatureActor.scala
+++ app/actors/TemperatureActor.scala
@@ -10,71 +10,114 @@ import java.io._
 import java.nio.file._
 import javax.inject.Inject
 import java.nio.charset.Charset
-
+import scala.util.Success
+import scala.util.Failure
 
 object TemperatureActor {
   def props = Props[TemperatureActor]
 
-  case class ReadTemperatures()
-  case class Temperatures(cpu: Double, ambient: Double, chamber: Option[Double])
+  case object ReadTemperatures
+  case class Temperatures(cpu: Option[Double], ambient: Option[Double], chamber: Option[Double])
 }
-
 
 class TemperatureActor @Inject() (configuration: Configuration) extends Actor {
   import context.dispatcher
   import TemperatureActor._
 
-  val channel = configuration.getInt("mcp3008.channel.tmp36").getOrElse(0)
+  val channel = configuration.getInt("mcp3008.channel.tmp36").getOrElse {
+    Logger.warn("Using default channel 0 for TMP36")
+    0
+  }
   val w1_device = setupOnewire
   val spi = new SpiWrapper
 
   def receive = {
     case ReadTemperatures => {
-      sender() ! Temperatures(readCpuTemp, readTmp36, readChamberTemp)
+      val sndr = sender()
+      val fermenter = Future { readChamberTemp }
+      val cpu = Future { readCpuTemp }
+      val ambient = Future { readTmp36 }
+
+      val resp = for {
+        f <- fermenter
+        c <- cpu
+        a <- ambient
+      } yield Temperatures(c, a, f)
+
+      resp onComplete {
+        case Success(t) => {
+          Logger.trace("Success!")
+          sndr ! t
+        }
+        case Failure(e) => {
+          Logger.trace("Failure!")
+          sndr ! Temperatures(None, None, None)
+        }
+      }
     }
   }
 
-  private def readCpuTemp = SystemInfo.getCpuTemperature.asInstanceOf[Double]
+  private def readCpuTemp = {
+    Logger.trace("reading CPU temp")
+    val tmp = SystemInfo.getCpuTemperature.asInstanceOf[Double]
+    Logger.info(s"CPU temp is $tmp")
+    Some(tmp)
+  }
 
   private def readTmp36 = {
     Logger.trace("reading TMP36 via SPI")
     val level = spi.readChannel(channel)
-    Logger.debug(s"level is $level")
+    Logger.debug(s"TMP36 level is $level")
     // convert level to temp in Celcius
-    ((level * 330.0) / 1023.0) - 50.0
+    val tmp = ((level * 330.0) / 1023.0) - 50.0
+    Logger.info(s"Ambient temp is $tmp")
+    Some(tmp)
   }
 
-  private def readChamberTemp : Option[Double] = {
-    try {
-      val path = FileSystems.getDefault.getPath(w1_device)
-      val lines = Files.readAllLines(path, Charset.forName("UTF-8"))
-      if (lines.size() == 2 && lines.get(0).endsWith("YES")) {
-        val second = lines.get(1)
-        val indx = second.indexOf("t=")
-        Some(second.substring(indx + 2).toDouble / 1000)
-      } else {
-        Thread.sleep(200L)
-        readChamberTemp
-      }
+  private def readChamberTemp: Option[Double] = {
+    Logger.trace("reading fermentation chamber temp")
+    w1_device match {
+      case Some(fname) =>
+        try {
+          val path = FileSystems.getDefault.getPath(fname)
+          val lines = Files.readAllLines(path, Charset.forName("UTF-8"))
+          if (lines.size() == 2 && lines.get(0).endsWith("YES")) {
+            val second = lines.get(1)
+            val indx = second.indexOf("t=")
+            val tmp = second.substring(indx + 2).toDouble / 1000
+            Logger.info(s"Chamber temp is $tmp")
+            Some(tmp)
+          } else {
+            Logger.info("Onewire read failed, trying again...")
+            Thread.sleep(200L)
+            readChamberTemp
+          }
+        } catch {
+          case e: IOException => {
+            Logger.error("Error reading fermentation temp from onewire", e)
+            None
+          }
+        }
+      case _ => None
     }
-    catch { case e: IOException => None }
   }
 
   private def setupOnewire = {
     val filename = configuration.getString("w1.device.filename")
+    Logger.debug(s"Fermentation chamber temp will be read from $filename")
     filename match {
       case Some(name) => {
         val path = FileSystems.getDefault.getPath(name)
         if (Files.exists(path) && Files.isReadable(path)) {
-          name
+          Some(name)
         } else {
           Logger.warn(s"Onewire device file $name does not exist or is not readable")
-          ""
+          None
         }
       }
       case None => {
         Logger.warn("Onewire device not configured")
-        ""
+        None
       }
     }
   }
--- app/controllers/TempController.scala
+++ app/controllers/TempController.scala
@@ -11,17 +11,34 @@ import akka.actor._
 import akka.pattern.ask
 import akka.util.Timeout
 import actors._
-import actors.TemperatureActor._
 
 @Singleton
 class TempController @Inject() (@Named("tmp-actor") tmpActor: ActorRef) extends Controller {
 
   def temperature = Action.async { implicit request =>
-    Logger.debug("request for ambient temp")
+    import actors.TemperatureActor._
+
+    Logger.debug("request for temp")
     implicit val timeout: Timeout = 5.seconds
     (tmpActor ? ReadTemperatures).mapTo[Temperatures].map { temp =>
-      val fahrenheit = (temp.ambient * 9/5) + 32
-      Logger.debug(s"Actor replied with $temp (ambient: $fahrenheit F)")
+      temp.ambient.map {
+        case a: Double => {
+          val fahrenheit = (a * 9/5) + 32
+          Logger.debug(s"Actor replied with ambient temp: $a ($fahrenheit F)")
+        }
+      }
+      temp.cpu.map {
+        case c: Double => {
+          val fahrenheit = (c * 9/5) + 32
+          Logger.debug(s"Actor replied with CPU temp: $c ($fahrenheit F)")
+        }
+      }
+      temp.chamber.map {
+        case f: Double => {
+          val fahrenheit = (f * 9/5) + 32
+          Logger.debug(s"Actor replied with chamber temp: $f ($fahrenheit F)")
+        }
+      }
       Ok(Json.obj(
           "cpu" -> temp.cpu,
           "ambient" -> temp.ambient,
